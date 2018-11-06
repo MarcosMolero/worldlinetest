913321110e36b75ef7756724daf7a90969cef397
Marcos Molero <marcosmolero@MacBook-Pro-de-Marcos.local>
2018-11-06 18:00:53 +0100

    Realm PointOI object allows to store detailed information of POI

--- worldlinetest/Models/PointOIRealm.swift
+++ worldlinetest/Models/PointOIRealm.swift
@@ -15,7 +15,12 @@ class PointOIRealm: Object {
     @objc dynamic var title: String?
     @objc dynamic var latitude: String?
     @objc dynamic var longitude: String?
-
+    @objc dynamic var address: String?
+    @objc dynamic var transport: String?
+    @objc dynamic var email: String?
+    @objc dynamic var desc: String?
+    @objc dynamic var phone: String?
+    
 //    init(id: String, title: String, latitude: String, longitude: String) {
 //        self.id = id
 //        self.title = title
